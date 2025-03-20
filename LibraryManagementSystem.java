486848d7b55eaa73e7bc93b2423973b40fc92ba6
kylezxc_ <114975031+kyleezxc@users.noreply.github.com>
2025-03-20 21:43:05 +0800

    Update LibraryManagementSystem.java

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -4,8 +4,9 @@ public class LibraryManagementSystem{
    public static void main(String[] args){
       Scanner scan = new Scanner(System.in);
       SinglyLinkedList bookList = new SinglyLinkedList();
+      DoublyLinkedList borrowList = new DoublyLinkedList();
       int option, bookID;
-      String bookTitle, bookAuthor;
+      String bookTitle, bookAuthor, bookBorrower;
       
       while(true){
          Menu();
@@ -22,7 +23,6 @@ public class LibraryManagementSystem{
                System.out.print("Enter Book Author: ");
                bookAuthor = scan.nextLine();
                bookList.addBook(bookID, bookTitle, bookAuthor);
-               System.out.println("Book '" + bookTitle + "' successfully added.");
             break;
             
             case 2:
@@ -35,7 +35,26 @@ public class LibraryManagementSystem{
                bookList.searchBook(bookTitle);
             break;
             
-            case 4:  
+            case 4:
+               if(bookList.isEmpty()){
+                  System.out.println("Library is empty, book not found !");
+                  break;
+               }
+               System.out.print("Enter Book Title to Borrow: ");
+               bookTitle = scan.next();
+               SinglyNode borrowed = bookList.removeBook(bookTitle);
+               if(borrowed == null){
+                  System.out.println("Book not found!");
+               }else{
+                  borrowList.borrowBook(borrowed);
+               }
+            break;
+
+            case 6:
+               borrowList.displayBorrowedBook();
+            break;
+
+            case 8:  
                System.out.println("Exiting program...");
             return;
          }
@@ -44,9 +63,13 @@ public class LibraryManagementSystem{
    
    public static void Menu(){
       System.out.println("\n--- Menu Library System ---");
-      System.out.println("1. Add Book");
-      System.out.println("2. Display Book");
-      System.out.println("3. Search Book");
-      System.out.println("4. Exit");
+      System.out.println("1. Add Book (Singly)");
+      System.out.println("2. Display Book Available (Singly)");
+      System.out.println("3. Search Book (Singly)");
+      System.out.println("4. Borrow Book (Doubly)");
+      System.out.println("5. Return Book (Doubly)");
+      System.out.println("6. Display Borrowed Book (Doubly)");
+      System.out.println("7. Track Borrow History (Stack)");
+      System.out.println("8. Exit");
    }
 }
